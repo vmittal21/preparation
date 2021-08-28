53b22c492ee44d95808b75bf9b68685c05939144
Vidhi Mittal <vmittal5@hawk.iit.edu>
2021-08-28 14:47:58 -0500

    added optimization

--- trapping_rain_water.py
+++ trapping_rain_water.py
@@ -46,3 +46,23 @@ class Solution:
                 
         return total_water  
 
+#using dynamic programming
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        maxL = [0]*len(height)
+        maxR = [0]*len(height)
+        maxL[0] = height[0]
+        maxR[-1] = height[len(height)-1]
+        total=0
+        
+        for i in range(1, len(height)):
+            maxL[i] = max(maxL[i-1], height[i])
+        
+        for i in range(len(height)-2, -1, -1):
+            maxR[i] = max(maxR[i+1], height[i])
+             
+        for i in range(0, len(height)):
+            total += min(maxL[i],maxR[i]) - height[i]
+            
+        return total
