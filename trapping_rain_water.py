0e165342a31c5726c7ea64d3f191b3a311e72c14
Vidhi Mittal <vmittal5@hawk.iit.edu>
2021-08-28 13:19:29 -0500

    added optimization

--- trapping_rain_water.py
+++ trapping_rain_water.py
@@ -16,3 +16,33 @@ class Solution:
              total_water += curr_water
              curr_loc +=1
         return total_water
+
+# optimization using 2 pointers approach:
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        
+        total_water = 0
+        right_ptr = len(height)-1
+        left_ptr = 0
+        maxL = 0
+        maxR=height[right_ptr]
+        
+        while left_ptr < right_ptr:
+                                       
+             if height[left_ptr]<=height[right_ptr]:
+                if height[left_ptr]>=maxL:
+                    maxL = height[left_ptr] 
+                else:
+                    total_water += maxL-height[left_ptr]
+                left_ptr+=1
+                
+             else:
+                 if height[right_ptr]>=maxR:
+                     maxR = height[right_ptr]   
+                 else:
+                     total_water += maxR-height[right_ptr]
+                 right_ptr-=1
+                
+        return total_water  
+
